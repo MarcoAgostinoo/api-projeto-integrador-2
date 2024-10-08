24449cd880ce67c3f38d81aaa8327f12cab18b38
Sandro Santos <sandro.santos@radialdc.local>
2024-10-08 05:48:16 -0300

    ajuste2cors0548

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://projeto-integrador-2-kappa.vercel.app'], 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    credentials: true 
+    origin: ['http://localhost:5173', 'https://projeto-integrador-2-kappa.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
 }));
 
 app.get('/', (req, res) => {
